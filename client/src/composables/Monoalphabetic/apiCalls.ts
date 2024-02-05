cf806e63e403cc13216f1c280b13c423c1ba9acd
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-05 20:03:14 +0100

    Defined interfaces for API calls for text requests

--- client/src/composables/Monoalphabetic/apiCalls.ts
+++ client/src/composables/Monoalphabetic/apiCalls.ts
@@ -1,23 +1,28 @@
 import axios from 'axios';
 
-export async function callAPI(action: string, headers?: any, parameters?: any) {
+export async function callAPI(action: string, parameters: NewTextRequestOptions) {
 
   switch (action) {
     case "text": {
-      const response = await axios.post("http://localhost:1337/monoalphabetic/text", headers, parameters);
+      const response = await axios.post("http://localhost:1337/monoalphabetic/text", parameters);
       return response.data;
     }
     case "hint": {
-      const response = await axios.post("http://localhost:1337/monoalphabetic/hint", headers, parameters);
+      const response = await axios.post("http://localhost:1337/monoalphabetic/hint", parameters);
       return response.data;
     }
     case "validation": {
-      const response = await axios.post("http://localhost:1337/monoalphabetic/validation", headers, parameters);
+      const response = await axios.post("http://localhost:1337/monoalphabetic/validation", parameters);
       return response.data;
     }
   }
 }
 
-export interface newTextObject {
+export interface NewTextResponse {
   newText: string
+}
+
+export interface NewTextRequestOptions {
+  keepSpaces: boolean,
+  keepPunctuation: boolean
 }
