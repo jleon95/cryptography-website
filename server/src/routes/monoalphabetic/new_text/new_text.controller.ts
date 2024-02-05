5bff0b2e22ea8d5b61d9971b423329cb59f579bf
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-05 13:55:41 +0100

    Implemented server-side new random text controller

--- server/src/routes/monoalphabetic/new_text/new_text.controller.ts
+++ server/src/routes/monoalphabetic/new_text/new_text.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from 'express';
+import { getNewText } from './new_text.service';
 
 const router = Router();
 
-router.post('/text', (req: Request, res: Response) => {
-  res.send('Hello, world.');
+router.post('/text', async (req: Request, res: Response) => {
+  const newText: string = await getNewText();
+  res.json({ newText });
 });
 
 export default router;
