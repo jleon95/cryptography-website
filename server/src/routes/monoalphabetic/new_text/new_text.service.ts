abfc425acb657693a4920202b147ff824fbef9b1
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-07 00:06:44 +0100

    Added several DB queries related to sessions and text encryption

--- server/src/routes/monoalphabetic/new_text/new_text.service.ts
+++ server/src/routes/monoalphabetic/new_text/new_text.service.ts
@@ -1,11 +1,13 @@
 import prisma from '../../../prisma/prisma-client';
+import type { LetterMapping } from './new_text.logic';
 
-interface IdQueryObject {
+export interface ChosenTextInfo {
+  text: string,
   id: number
 }
 
-export async function getNewText() {
-  const ids: IdQueryObject[] = await prisma.originalText.findMany({
+export async function chooseNewText(): Promise<ChosenTextInfo> {
+  const ids: {id: number}[] = await prisma.originalText.findMany({
     select: {
       id: true,
       content: false,
@@ -22,5 +24,56 @@ export async function getNewText() {
     },
   })).content;
 
-  return chosenText;
+  return { text: chosenText, id: chosenId };
+}
+
+export async function insertSession(sessionId: string, maxAge: number, data) {
+  await prisma.session.create({
+    data: {
+      id: sessionId,
+      expiresAt: new Date((new Date().getTime()) + maxAge),
+      data: data
+    }
+  });
+}
+
+export async function touchSession(sessionId: string, maxAge: number) {
+  await prisma.session.update({
+    where: {
+      id: sessionId
+    },
+    data: {
+      expiresAt: new Date((new Date().getTime()) + maxAge)
+    }
+  })
+}
+
+export async function deleteSession(sessionId: string) {
+  await prisma.session.delete({
+    where: {
+      id: sessionId
+    }
+  });
+}
+
+export async function insertTextToBeDecrypted(letterMapping: LetterMapping, originalTextId: number, sessionId: string, deletePreviousEncryptedText: boolean = false) {
+
+  if (deletePreviousEncryptedText)
+    await deleteTextToBeDecryptedBySessionId(sessionId);
+
+  await prisma.textBeingDecrypted.create({
+    data: {
+      encryptionMapping: letterMapping,
+      originalTextId: originalTextId,
+      sessionId: sessionId
+    }
+  });
+}
+
+export async function deleteTextToBeDecryptedBySessionId(sessionId: string) {
+  await prisma.textBeingDecrypted.delete({
+    where: {
+      sessionId: sessionId
+    }
+  });
 }
