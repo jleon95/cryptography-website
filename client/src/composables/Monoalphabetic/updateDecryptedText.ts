7c36e06d98ddeebec4bf3add61383a5883211158
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-08 17:51:07 +0100

    Extracted keyDown event handler from Vue component and also implemented keyUp event handler

---
+++ client/src/composables/Monoalphabetic/updateDecryptedText.ts
@@ -0,0 +1,21 @@
+﻿import { useTextStore } from './textStore';
+
+const allowedLetterInputSet: Set<string> = new Set("abcdefghijklmnñopqrstuvwxyz");
+const allowedInputSet: Set<string> = new Set("abcdefghijklmnñopqrstuvwxyz");
+allowedInputSet.add("Backspace");
+
+export function processKeyDown(e: KeyboardEvent) {
+  if (!allowedInputSet.has(e.key) || (e.currentTarget as HTMLElement).textContent!.length > 0 && e.key != "Backspace")
+    e.preventDefault();
+}
+
+export function processKeyUp(e: KeyboardEvent) {
+
+  const textStore = useTextStore();
+  const originalLetter: string = (e.currentTarget as HTMLElement).previousSibling!.textContent!;
+
+  if (allowedLetterInputSet.has(e.key))
+    textStore.assignedLetters[originalLetter] = e.key
+  else if (e.key == "Backspace")
+    textStore.assignedLetters[originalLetter] = originalLetter;
+}
