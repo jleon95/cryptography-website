065adb4c18b045468d51fcdfd6374cc23cdfcd6f
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-08 13:55:10 +0100

    Re-implemented computed value letterFrequencies with map() and reduce()

--- client/src/composables/Monoalphabetic/textStore.ts
+++ client/src/composables/Monoalphabetic/textStore.ts
@@ -10,21 +10,18 @@ export const useTextStore = defineStore('text', () => {
   const expirationDate = ref(0); // Stored in milliseconds since 1970 blah blah blah to circumvent weird Date operations.
   const letterFrequencies = computed(() => {
     
-    const letterFrequencies: { [letter: string]: number } = {};
+    // Initialize all counters to 0
     let total: number = 0;
-
-    for (const letter of letters)
-      letterFrequencies[letter] = 0;
-
-    for (const letter of encryptedText.value)
+    const letterFrequencies: { [letter: string]: number } = letters.split("").reduce((obj, letter) => ({ ...obj, [letter]: 0 }), {});
+    // Count appearances in text
+    encryptedText.value.split("").map((letter) => {
       if (letter in letterFrequencies) {
         letterFrequencies[letter]++;
         total++;
       }
-
+    });
     // Percentages rounded to 2 decimal places.
-    for (const letter in letterFrequencies)
-      letterFrequencies[letter] = +((letterFrequencies[letter] * 100 / total).toFixed(2));
+    letters.split("").map((letter) => letterFrequencies[letter] = +((letterFrequencies[letter] * 100 / total).toFixed(2)));
 
     return letterFrequencies;
   });
