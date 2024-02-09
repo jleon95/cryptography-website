8cc61672a29e763f6eb5f6e0337400ee34dbf85a
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-09 14:29:34 +0100

    Improved type checking and 'const' usage

--- server/src/routes/monoalphabetic/controller.models.ts
+++ server/src/routes/monoalphabetic/controller.models.ts
@@ -1,3 +1,14 @@
+// Format of new text requests from the front-end
+export interface NewTextRequest {
+  difficultyOptions: {
+    keepSpaces: boolean,
+    keepPunctuation: boolean
+  },
+  sessionData: {
+    sessionId: string,
+  }
+}
+
 // JSONify-able response body for new text requests from the front-end
 export interface NewTextResponse {
   sessionData: {
--- server/src/routes/monoalphabetic/new_text/new_text.controller.ts
+++ server/src/routes/monoalphabetic/new_text/new_text.controller.ts
@@ -4,7 +4,7 @@ import { createNewEncryptedText } from './new_text.logic';
 import { chooseNewText } from './new_text.service';
 import type { EncryptedTextInfo } from '../logic.models';
 import type { ChosenOriginalTextInfo } from '../service.models';
-import type { NewTextResponse } from '../controller.models'
+import type { NewTextRequest, NewTextResponse } from '../controller.models'
 const logger = require('../../../../logger');
 const crypto = require("crypto");
 const router = Router();
@@ -14,35 +14,34 @@ function createExpirationDate(): Date {
 }
 
 function createSession() {
-  const sessionId: string = crypto.randomUUID();
-  const expirationDate: Date = createExpirationDate();
-  return { sessionId, expirationDate };
+  return { sessionId: crypto.randomUUID(), expirationDate: createExpirationDate() };
 }
 
 router.post('/new_text', async (req: Request, res: Response) => {
 
-  let chosenTextInfo: ChosenOriginalTextInfo = await chooseNewText();
-  let encryptedTextInfo: EncryptedTextInfo = await createNewEncryptedText(chosenTextInfo.text, req.body.difficultyOptions);
+  const requestBody: NewTextRequest = req.body;
+  const chosenTextInfo: ChosenOriginalTextInfo = await chooseNewText();
+  const encryptedTextInfo: EncryptedTextInfo = await createNewEncryptedText(chosenTextInfo.text, requestBody.difficultyOptions);
 
-  if (req.body.sessionData.sessionId === "") {
-    let newSession = createSession();
+  if (requestBody.sessionData.sessionId === "") {
+    const newSession = createSession();
     const childLogger = logger.child({ sessionId: newSession.sessionId });
     childLogger.trace("Creating new encrypted text for new session ID.");
     await insertSession(newSession.sessionId, newSession.expirationDate, {});
     insertTextToBeDecrypted(encryptedTextInfo.letterMapping, chosenTextInfo.id, newSession.sessionId, false);
-    let responseBody: NewTextResponse = {
+    const responseBody: NewTextResponse = {
       sessionData: newSession,
       encryptedText: encryptedTextInfo.text
     };
     res.json(responseBody);
   }
-  else if (typeof req.body.sessionData.sessionId === "string" && await checkSessionExists(req.body.sessionData.sessionId)) {
-    let newExpirationDate: Date = createExpirationDate();
-    const childLogger = logger.child({ sessionId: req.body.sessionData.sessionId });
+  else if (typeof requestBody.sessionData.sessionId === "string" && await checkSessionExists(requestBody.sessionData.sessionId)) {
+    const newExpirationDate: Date = createExpirationDate();
+    const childLogger = logger.child({ sessionId: requestBody.sessionData.sessionId });
     childLogger.trace("Creating new encrypted text for existing session ID.");
-    touchSession(req.body.sessionData.sessionId, newExpirationDate);
-    insertTextToBeDecrypted(encryptedTextInfo.letterMapping, chosenTextInfo.id, req.body.sessionData.sessionId, true);
-    let responseBody: NewTextResponse = {
+    touchSession(requestBody.sessionData.sessionId, newExpirationDate);
+    insertTextToBeDecrypted(encryptedTextInfo.letterMapping, chosenTextInfo.id, requestBody.sessionData.sessionId, true);
+    const responseBody: NewTextResponse = {
       sessionData: {
         expirationDate: newExpirationDate
       },
@@ -51,7 +50,7 @@ router.post('/new_text', async (req: Request, res: Response) => {
     res.json(responseBody);
   }
   else {
-    const childLogger = logger.child({ sessionId: req.body.sessionData.sessionId });
+    const childLogger = logger.child({ sessionId: requestBody.sessionData.sessionId });
     childLogger.warn("Unrecognized session ID in API request for creation of new encrypted text, sending error 400 instead.");
     res.status(400).send("Unrecognized session ID");
   }
