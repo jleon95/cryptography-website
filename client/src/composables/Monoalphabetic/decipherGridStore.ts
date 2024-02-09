9e1b238dd89fd8c07fc6cf16bbabdf4cb2363fad
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-09 19:30:14 +0100

    Implemented pinia store to manage the state of the letter deciphering grid

---
+++ client/src/composables/Monoalphabetic/decipherGridStore.ts
@@ -0,0 +1,50 @@
+﻿import { defineStore } from 'pinia';
+import { reactive } from 'vue';
+
+export enum CellState {
+  CORRECT = "correct",
+  WRONG = "wrong",
+  DEFAULT = ""
+}
+
+const defaultState = {
+  contentCellStyles: {
+    correct: false,
+    wrong: false
+  }
+};
+
+interface CellStyleClasses {
+  correct: boolean,
+    wrong: boolean
+}
+
+const letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+export const useDecipherGridStore = defineStore("decipherGrid", () => {
+
+  const contentCellStyleClasses: { [letter: string]: CellStyleClasses } = reactive(letters.split("").reduce((obj, letter) => ({ ...obj, [letter]: { ...defaultState.contentCellStyles } }), {}));
+
+  function updateCellState(letter: string, newState: CellState): void {
+
+    switch (newState) {
+      case CellState.CORRECT: {
+        contentCellStyleClasses[letter].correct = true;
+        contentCellStyleClasses[letter].wrong = false;
+        break;
+      }
+      case CellState.WRONG: {
+        contentCellStyleClasses[letter].correct = false;
+        contentCellStyleClasses[letter].wrong = true;
+        break;
+      }
+      case CellState.DEFAULT: {
+        contentCellStyleClasses[letter].correct = false;
+        contentCellStyleClasses[letter].wrong = false;
+        break;
+      }
+    }
+  }
+
+  return { contentCellStyleClasses, updateCellState };
+});
