7776b8b26d6d518903e84d23b513112d87993649
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-18 01:17:34 +0100

    Added if statement to prevent redundant reveal text requests, and disabled non-validated cells when response is received

--- client/src/composables/Monoalphabetic/ButtonFunctionality/displaySolution.ts
+++ client/src/composables/Monoalphabetic/ButtonFunctionality/displaySolution.ts
@@ -1,6 +1,7 @@
 import { useSessionStore } from '../Stores/sessionStore';
 import { useGameProgressStore } from '../Stores/gameProgressStore';
 import { useTextStore } from '../Stores/textStore';
+import { useDecipherGridDOMStatesStore, CellState } from '../Stores/decipherGridDOMStatesStore';
 import { callAPI, Action } from '../apiCalls';
 import type { RevealTextRequest, RevealTextResponse } from '../apiCalls';
 
@@ -16,10 +17,12 @@ export async function displaySolution() {
         sessionId: sessionStore.sessionId
       }
     };
-    const originalText: string = (await callAPI(Action.REVEAL_TEXT, revealTextRequestBody) as RevealTextResponse).originalText;
     const textStore = useTextStore();
-    textStore.originalText = originalText;
-
+    textStore.originalText = (await callAPI(Action.REVEAL_TEXT, revealTextRequestBody) as RevealTextResponse).originalText;
     gameProgressStore.isSolutionRevealed = true;
+    const decipherGridDOMStatesStore = useDecipherGridDOMStatesStore();
+    for (const letter in decipherGridDOMStatesStore.contentCellStyleClasses)
+      if (!decipherGridDOMStatesStore.contentCellStyleClasses[letter].correct && !decipherGridDOMStatesStore.contentCellStyleClasses[letter].hint)
+        decipherGridDOMStatesStore.updateCellState(letter, CellState.DISABLED);
   }
 }
