c52acc5917739d2c2f05811eb40079771b6254ba
Javier León Palomares <jleon95@users.noreply.github.com>
2024-02-18 19:59:24 +0100

    Reused function from utils.ts to remove duplicate code

--- client/src/composables/Monoalphabetic/ButtonFunctionality/changeTextSettings.ts
+++ client/src/composables/Monoalphabetic/ButtonFunctionality/changeTextSettings.ts
@@ -1,24 +1,23 @@
 import { useSessionStore } from '../Stores/sessionStore';
 import { useTextStore } from '../Stores/textStore';
 import { callAPI, Action } from '../apiCalls';
+import { resetAnimationsOfElement } from '../utils';
 import type { UpdateTextRequest, UpdateTextResponse } from '../apiCalls';
 
 function resetTextSettingsStyles() {
   const textSettingsContainer: HTMLElement = document.getElementById("text-settings-container")!;
   const closeTextSettingsButton: HTMLElement = document.getElementById("close-text-settings-button")!;
   textSettingsContainer.style.setProperty("opacity", window.getComputedStyle(textSettingsContainer).getPropertyValue("opacity"));
-  textSettingsContainer.classList.remove("inflate-text-settings");
-  textSettingsContainer.classList.add("deflate-text-settings");
+  resetAnimationsOfElement(textSettingsContainer, "inflate-text-settings", "deflate-text-settings");
   closeTextSettingsButton.style.setProperty("opacity", window.getComputedStyle(closeTextSettingsButton).getPropertyValue("opacity"));
-  closeTextSettingsButton.classList.remove("inflate-button");
-  closeTextSettingsButton.classList.add("deflate-button");
+  resetAnimationsOfElement(closeTextSettingsButton, "inflate-button", "deflate-button");
   setTimeout(function () {
-    textSettingsContainer.classList.remove("deflate-text-settings");
     textSettingsContainer.style.removeProperty("display");
     textSettingsContainer.style.removeProperty("opacity");
-    closeTextSettingsButton.classList.remove("deflate-button");
+    resetAnimationsOfElement(textSettingsContainer, "deflate-text-settings");
     closeTextSettingsButton.style.removeProperty("display");
     closeTextSettingsButton.style.removeProperty("opacity");
+    resetAnimationsOfElement(closeTextSettingsButton, "deflate-button");
   }, 700);
 }
 
@@ -75,10 +74,6 @@ export function deployTextSettings() {
   const closeTextSettingsButton: HTMLElement = document.getElementById("close-text-settings-button")!;
   (document.getElementById("keep-spaces-checkbox") as HTMLInputElement).checked = sessionStore.activeTextSettings.keepSpaces;
   (document.getElementById("keep-punctuation-checkbox") as HTMLInputElement).checked = sessionStore.activeTextSettings.keepPunctuation;
-  if (textSettingsContainer.classList.contains("deflate-text-settings"))
-    textSettingsContainer.classList.remove("deflate-text-settings");
-  textSettingsContainer.classList.add("inflate-text-settings");
-  if (closeTextSettingsButton.classList.contains("deflate-button"))
-    closeTextSettingsButton.classList.remove("deflate-button");
-  closeTextSettingsButton.classList.add("inflate-button");
+  resetAnimationsOfElement(textSettingsContainer, "deflate-text-settings", "inflate-text-settings");
+  resetAnimationsOfElement(closeTextSettingsButton, "deflate-button", "inflate-button");
 }
