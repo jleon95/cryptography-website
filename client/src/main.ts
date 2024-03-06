8f532434e1009fd95f774a91aee6e7b8dfd5a2fa
Javier León Palomares <jleon95@users.noreply.github.com>
2024-03-06 13:44:00 +0100

    Added vue router to main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,27 @@
 import './assets/main.css'
 
 import { createPinia } from 'pinia';
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import * as VueRouter from 'vue-router';
+import MainPage from './components/MainPage.vue';
+import MonoalphabeticApp from './components/MonoalphabeticApp.vue';
+
+const Home = { template: MainPage };
+const Monoalphabetic = { template: MonoalphabeticApp };
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/monoalphabetic", component: Monoalphabetic },
+];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes
+});
 
 const pinia = createPinia();
-const app = createApp(App);
+const app = createApp(MainPage);
 
+app.use(router);
 app.use(pinia);
 app.mount('#app');
