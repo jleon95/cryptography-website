25601d8b13657ac294717fb0bac0306b00ee8d46
Javier León Palomares <jleon95@users.noreply.github.com>
2024-03-08 16:10:07 +0100

    Added hexagonal buttons to main page

--- client/src/components/MainPage/RoutingButtons.vue
+++ client/src/components/MainPage/RoutingButtons.vue
@@ -1,20 +1,15 @@
-﻿<template>
-  <router-link to="/monoalphabetic">
-    <div class="card">
-      <div class="header">
-        <h1>1</h1>
-      </div>
-      <div class="container">
-        <p>Cifrado monoalfabético</p>
-      </div>
-    </div>
-  </router-link>
-  <div class="card">
-    <div class="header">
-      <h1>2</h1>
-    </div>
+<template>
+  <div class="main">
     <div class="container">
-      <p>Cifrado por transposición</p>
+      <router-link to="/monoalphabetic">
+        <div class="material-symbols-outlined material-icons md-28">swap_horiz</div>
+      </router-link>
+      <a>
+        <div class="material-symbols-outlined material-icons md-28 disabled">donut_large</div>
+      </a>
+      <a>
+        <div class="material-symbols-outlined material-icons md-28">question_mark</div>
+      </a>
     </div>
   </div>
 </template>
@@ -24,29 +19,54 @@
     text-decoration: none;
   }
 
-  div.card {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    margin: 0 1rem;
-    overflow: hidden;
-    text-align: center;
-    transition: all 0.2s;
-    width: 250px;
+  /* Source: https://css-tricks.com/hexagons-and-beyond-flexible-responsive-grid-patterns-sans-media-queries/ */
+  .main {
+    display: flex;
+    filter: drop-shadow(0 8px 6px rgba(0, 0, 0, 0.5));
+    max-width: 180px;
+    --f: calc(var(--s) * 1.732 + 4 * var(--m) - 1px);
+    --m: 5px; /* margin */
+    --s: 80px; /* size  */
   }
 
-  div.card:hover {
-    transform: scale(1.1, 1.1);
+  .container {
+    font-size: 0; /* disable white space between inline block element */
   }
 
-  div.header {
-    background-color: var(--color-mainpage-button-card-background);
-    color: var(--color-mainpage-button-card-text);
-    font-size: 2.5rem;
-    padding: 10px;
+  .container div {
+    align-items: center;
+    background-color: var(--base-dark-primary);
+    clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
+    color: white;
+    display: inline-flex;
+    font-size: initial;
+    height: calc(var(--s) * 1.1547);
+    justify-content: center;
+    margin: var(--m);
+    margin-bottom: calc(var(--m) - var(--s) * 0.2885);
+    transition: all ease 0.5s;
+    width: var(--s);
   }
 
-  div.container {
-    background-color: var(--color-mainpage-button-caption-background);
-    color: var(--color-mainpage-button-caption-text);
-    padding: 1rem;
+  .container div:hover {
+    background-color: var(--base-secondary);
+    transform: rotate(360deg);
+  }
+
+  .container div.disabled {
+    pointer-events: none;
+    background-color: var(--base-mute-primary);
+  }
+
+  .container::before {
+    content: "";
+    float: left;
+    height: 120%;
+    shape-outside: repeating-linear-gradient(#0000 0 calc(var(--f) - 3px), #000 0 var(--f));
+    width: calc(var(--s) / 2 + var(--m));
+  }
+
+  .material-icons.md-28 {
+    font-size: 28px;
   }
 </style>
