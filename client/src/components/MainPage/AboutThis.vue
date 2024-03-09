7e75090cc8f7e5a81e0c6e65b6d3906fa4163404
Javier León Palomares <jleon95@users.noreply.github.com>
2024-03-09 21:35:42 +0100

    Added 'about this webpage' component in main page

---
+++ client/src/components/MainPage/AboutThis.vue
@@ -0,0 +1,90 @@
+﻿<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="about-this-webpage" class="about-this-webpage">
+    <span id="about-this-webpage-text" class="about-this-webpage-text">
+      El propósito de esta página es mostrar interactivamente algunos conceptos del campo de la criptografía. En los demás botones encontrarás los minijuegos disponibles.
+    </span>
+   </div>
+</template>
+
+<style scoped>
+  .about-this-webpage {
+    align-items: center;
+    background-color: var(--base-black-mute);
+    border-radius: 0.5rem;
+    color: white;
+    display: flex;
+    justify-content: center;
+    left: 50%;
+    max-width: 33%;
+    padding: 1rem;
+    position: absolute;
+    text-align: justify;
+    top: 150%;
+    transform: translate(-50%, 0) scale(0,1);
+    width: fit-content;
+  }
+
+  .deploy-about-this-webpage {
+    animation-delay: 0.5s;
+    animation-duration: 15s;
+    animation-name: deploy-about-this-webpage;
+    animation-timing-function: ease;
+  }
+
+  .deploy-about-this-webpage-text {
+    animation-delay: 0.5s;
+    animation-duration: 15s;
+    animation-name: deploy-about-this-webpage-text;
+    animation-timing-function: ease;
+  }
+
+  @keyframes deploy-about-this-webpage {
+    0% {
+      transform: translate(-50%, 0) scale(0,1);
+    }
+    5% {
+      box-shadow: none;
+      transform: translate(-50%, 0) scale(1,1);
+    }
+    10% {
+      box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.6), 0 6px 20px 0 rgba(0, 0, 0, 0.5);
+      transform: translate(-50%, 0) scale(1,1);
+    }
+    90% {
+      box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.6), 0 6px 20px 0 rgba(0, 0, 0, 0.5);
+      transform: translate(-50%, 0) scale(1,1);
+    }
+    95% {
+      box-shadow: none;
+      transform: translate(-50%, 0) scale(1,1);
+    }
+    100% {
+      transform: translate(-50%, 0) scale(0,1);
+    }
+  }
+
+  @keyframes deploy-about-this-webpage-text {
+    0% {
+      opacity: 0;
+    }
+    5% {
+      opacity: 0;
+    }
+    10% {
+      opacity: 1;
+    }
+    90% {
+      opacity: 1;
+    }
+    95% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+</style>
--- client/src/components/MainPage/RoutingButtons.vue
+++ client/src/components/MainPage/RoutingButtons.vue
@@ -1,20 +1,44 @@
 ﻿<script setup lang="ts">
   import RoutingButton from './RoutingButton.vue';
+  import AboutThis from './AboutThis.vue';
+  import { resetAnimationsOfElement } from '../../composables/Monoalphabetic/utils';
+
+  function deployAboutThis() {
+    const aboutThisWebPage: HTMLElement = document.getElementById("about-this-webpage")!;
+    const aboutThisWebPageText: HTMLElement = document.getElementById("about-this-webpage-text")!;
+    resetAnimationsOfElement(aboutThisWebPage, "deploy-about-this-webpage", "deploy-about-this-webpage");
+    resetAnimationsOfElement(aboutThisWebPageText, "deploy-about-this-webpage-text", "deploy-about-this-webpage-text");
+    setTimeout(() => {
+      resetAnimationsOfElement(aboutThisWebPage, "deploy-about-this-webpage");
+      resetAnimationsOfElement(aboutThisWebPageText, "deploy-about-this-webpage-text");
+    }, 15500);
+  }
 </script>
 
 <template>
-  <div class="main">
-    <div class="container">
-      <RoutingButton icon="swap_horiz" tooltip-position="above" tooltip="Cifrado monoalfabético" state="" path="/monoalphabetic" />
-      <RoutingButton icon="donut_large" tooltip-position="above" tooltip="Cifrado por transposición" state="disabled" path="" />
-      <RoutingButton icon="question_mark" tooltip-position="below" tooltip="Acerca de esta página" state="" path="" />
+  <div class="content">
+    <div class="hexagon-grid">
+      <div class="button-container">
+        <RoutingButton icon="swap_horiz" tooltip-position="above" tooltip="Cifrado monoalfabético" state="" path="/monoalphabetic" />
+        <RoutingButton icon="donut_large" tooltip-position="above" tooltip="Cifrado por transposición" state="disabled" path="" />
+        <RoutingButton @click="deployAboutThis" icon="question_mark" tooltip-position="below" tooltip="Acerca de esta página" state="" path="" />
+      </div>
     </div>
+    <AboutThis />
   </div>
 </template>
 
 <style scoped>
+  .content {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    width: 100%;
+  }
+
   /* Source: https://css-tricks.com/hexagons-and-beyond-flexible-responsive-grid-patterns-sans-media-queries/ */
-  .main {
+  .hexagon-grid {
     display: flex;
     filter: drop-shadow(0 8px 6px rgba(0, 0, 0, 0.5));
     max-width: 180px;
@@ -23,11 +47,11 @@
     --s: 80px; /* size  */
   }
 
-  .container {
+  .button-container {
     font-size: 0; /* disable white space between inline block element */
   }
 
-  .container::before {
+  .button-container::before {
     content: "";
     float: left;
     height: 120%;
