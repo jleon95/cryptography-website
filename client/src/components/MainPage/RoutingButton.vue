9d75f80d391e60c859f406962e84bff67d275c89
Javier León Palomares <jleon95@users.noreply.github.com>
2024-03-09 01:53:11 +0100

    Extracted main page button code to a separate component

---
+++ client/src/components/MainPage/RoutingButton.vue
@@ -0,0 +1,119 @@
+﻿<script setup lang="ts">
+  defineProps<{
+    icon: string,
+    tooltipPosition: string,
+    tooltip: string,
+    path: string,
+    state: string
+  }>()
+</script>
+
+<template>
+  <router-link class="tooltip" :class="state" :to="{path: path}">
+    <span class="icon material-symbols-outlined material-icons md-28">{{ icon }}</span>
+    <span class="tooltiptext" :class="tooltipPosition">{{ tooltip }}</span>
+  </router-link>
+</template>
+
+<style scoped>
+  a { /*<router-link> is converted to <a>*/
+    text-decoration: none;
+  }
+
+  .tooltip {
+    align-items: center;
+    display: inline-flex;
+    font-size: initial;
+    justify-content: center;
+    margin: var(--m);
+    margin-bottom: calc(var(--m) - var(--s) * 0.2885);
+    position: relative;
+  }
+
+  .icon {
+    align-items: center;
+    background-color: var(--base-dark-primary);
+    clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
+    color: white;
+    display: inline-flex;
+    height: calc(var(--s) * 1.1547);
+    justify-content: center;
+    width: var(--s);
+    transition: all ease 0.5s;
+  }
+
+  .tooltip:hover > .icon {
+    background-color: var(--base-secondary);
+    transform: rotate(360deg);
+  }
+
+  .tooltip.disabled {
+    pointer-events: none;
+  }
+
+  .tooltip.disabled > .icon {
+    background-color: var(--base-mute-primary);
+  }
+
+  .tooltiptext {
+    background-color: transparent;
+    border-radius: 0.5rem;
+    color: transparent;
+    display: inline-block;
+    left: 50%;
+    padding: 0.5rem 1rem;
+    position: absolute;
+    text-align: center;
+    transition: background-color ease 0.6s, color ease 0.6s;
+    transition-delay: 1s;
+    visibility: hidden;
+    white-space: nowrap;
+    z-index: 1;
+  }
+
+  .tooltiptext.above {
+    top: -60%;
+    transform: translate(-50%, 0);
+  }
+
+  .tooltiptext.below {
+    bottom: -60%;
+    transform: translate(-50%, 0);
+  }
+
+  .tooltiptext::after {
+    border-color: transparent transparent transparent transparent;
+    border-style: solid;
+    border-width: 0.4rem;
+    content: "";
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%, 0%);
+    transition: border-color ease 0.6s;
+    transition-delay: 1s;
+  }
+
+  .tooltiptext.above::after {
+    top: 100%;
+  }
+
+  .tooltiptext.below::after {
+    bottom: 100%;
+  }
+
+  .tooltip:hover .tooltiptext {
+    background-color: var(--color-toolbar-button-tooltip-background);
+    color: var(--color-toolbar-button-tooltip-text);
+    visibility: visible;
+  }
+
+  .tooltip:hover .above::after {
+    border-color: var(--color-toolbar-button-tooltip-border) transparent transparent transparent;
+    visibility: visible;
+  }
+
+  .tooltip:hover .below::after {
+    border-color: transparent transparent var(--color-toolbar-button-tooltip-border) transparent;
+    visibility: visible;
+  }
+</style>
--- client/src/components/MainPage/RoutingButtons.vue
+++ client/src/components/MainPage/RoutingButtons.vue
@@ -1,24 +1,18 @@
+﻿<script setup lang="ts">
+  import RoutingButton from './RoutingButton.vue';
+</script>
+
 <template>
   <div class="main">
     <div class="container">
-      <router-link class="material-symbols-outlined material-icons md-28" to="/monoalphabetic">
-        swap_horiz
-      </router-link>
-      <a class="material-symbols-outlined material-icons md-28 disabled">
-        donut_large
-      </a>
-      <a class="material-symbols-outlined material-icons md-28">
-        question_mark
-      </a>
+      <RoutingButton icon="swap_horiz" tooltip-position="above" tooltip="Cifrado monoalfabético" state="" path="/monoalphabetic" />
+      <RoutingButton icon="donut_large" tooltip-position="above" tooltip="Cifrado por transposición" state="disabled" path="" />
+      <RoutingButton icon="question_mark" tooltip-position="below" tooltip="Acerca de esta página" state="" path="" />
     </div>
   </div>
 </template>
 
 <style scoped>
-  a { /*<router-link> is converted to <a>*/
-    text-decoration: none;
-  }
-
   /* Source: https://css-tricks.com/hexagons-and-beyond-flexible-responsive-grid-patterns-sans-media-queries/ */
   .main {
     display: flex;
@@ -31,31 +25,6 @@
 
   .container {
     font-size: 0; /* disable white space between inline block element */
-  }
-
-  .container a {
-    align-items: center;
-    background-color: var(--base-dark-primary);
-    clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
-    color: white;
-    display: inline-flex;
-    font-size: initial;
-    height: calc(var(--s) * 1.1547);
-    justify-content: center;
-    margin: var(--m);
-    margin-bottom: calc(var(--m) - var(--s) * 0.2885);
-    transition: all ease 0.5s;
-    width: var(--s);
-  }
-
-  .container a:hover {
-    background-color: var(--base-secondary);
-    transform: rotate(360deg);
-  }
-
-  .container a.disabled {
-    pointer-events: none;
-    background-color: var(--base-mute-primary);
   }
 
   .container::before {
