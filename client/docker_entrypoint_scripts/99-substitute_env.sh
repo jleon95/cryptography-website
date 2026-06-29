c268ab274b0733c8f6a2ab0da0476eba43491d27
Javi <10140054+jleon95@users.noreply.github.com>
2026-06-29 13:38:43 +0000

    Added comments to explain why the environment substitution script is needed

--- client/docker_entrypoint_scripts/99-substitute_env.sh
+++ client/docker_entrypoint_scripts/99-substitute_env.sh
@@ -1,2 +1,4 @@
+# This script substitutes placeholder variables in the frontend assets with their correct runtime values.
+# It is needed at the deployment stage because the frontend is already bundled and inside a pre-built image.
 sed -i "s|#{backend_base_url}#|${BACKEND_ADDRESS}|g" /usr/share/nginx/html/assets/*.js
 sed -i "s|#{max_hints}#|${MAX_HINTS}|g" /usr/share/nginx/html/assets/*.js
