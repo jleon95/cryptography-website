308c47d3553df6d3672e3cc693f1c3e4ac5a77b0
Javi <10140054+jleon95@users.noreply.github.com>
2026-07-02 00:21:09 +0000

    Implemented startNewSession service method

--- server/src/minigames/monoalphabetic/services/monoalphabetic.service.ts
+++ server/src/minigames/monoalphabetic/services/monoalphabetic.service.ts
@@ -1,5 +1,6 @@
 import type { LetterMapping, ValidatedLetterMapping } from "../dtos/logic.dto.js";
 import type { IMonoalphabeticRepository } from "../repositories/interfaces/i-monoalphabetic.repository.js";
+import { createExpirationDate, createNewEncryptedText } from "./monoalphabetic.logic.js";
 
 export type StartNewSessionResponse = {
   newText: string;
@@ -13,7 +14,25 @@ export class MonoalphabeticService {
     keepSpaces: boolean,
     keepPunctuation: boolean,
     previousSessionId?: string,
-  ): Promise<StartNewSessionResponse> {}
+  ): Promise<StartNewSessionResponse> {
+    if (typeof previousSessionId !== "undefined") {
+      await this.monoalphabeticRepository.deleteSession(previousSessionId);
+    }
+
+    const numberOfTextsInDatabase =
+      await this.monoalphabeticRepository.getNumberOfTextsInDatabase();
+    const randomTextIndex = Math.floor(Math.random() * numberOfTextsInDatabase) + 1;
+    const newText = await this.monoalphabeticRepository.getTextByOffset(randomTextIndex);
+    const encryptedTextInfo = createNewEncryptedText(newText, keepSpaces, keepPunctuation);
+    const newSessionId = crypto.randomUUID();
+    await this.monoalphabeticRepository.createSession(
+      newSessionId,
+      createExpirationDate(),
+      randomTextIndex,
+      encryptedTextInfo.letterMapping,
+    );
+    return { newText: encryptedTextInfo.encryptedText, newSessionId };
+  }
 
   public async generateHint(sessionId: string): Promise<string> {}
 
