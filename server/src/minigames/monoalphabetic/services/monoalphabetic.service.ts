64a62813ef04a1b2121af94ab37978bdf1f9a64f
Javi <10140054+jleon95@users.noreply.github.com>
2026-07-02 02:02:24 +0000

    Implemented generateHint service method

--- server/src/minigames/monoalphabetic/services/monoalphabetic.service.ts
+++ server/src/minigames/monoalphabetic/services/monoalphabetic.service.ts
@@ -1,6 +1,15 @@
+import { MonoalphabeticNoHintsLeftError } from "../../../core/errors/errors.js";
+import { env as validEnv } from "../../../env.js";
 import type { LetterMapping, ValidatedLetterMapping } from "../dtos/logic.dto.js";
-import type { IMonoalphabeticRepository } from "../repositories/interfaces/i-monoalphabetic.repository.js";
-import { createExpirationDate, createNewEncryptedText } from "./monoalphabetic.logic.js";
+import type {
+  GetSessionResponse,
+  IMonoalphabeticRepository,
+} from "../repositories/interfaces/i-monoalphabetic.repository.js";
+import {
+  createExpirationDate,
+  createNewEncryptedText,
+  findCorrectLetterFromMapping,
+} from "./monoalphabetic.logic.js";
 
 export type StartNewSessionResponse = {
   newText: string;
@@ -34,7 +43,24 @@ export class MonoalphabeticService {
     return { newText: encryptedTextInfo.encryptedText, newSessionId };
   }
 
-  public async generateHint(sessionId: string): Promise<string> {}
+  public async generateHint(sessionId: string, requestedLetter: string): Promise<string> {
+    const currentSession: GetSessionResponse =
+      await this.monoalphabeticRepository.getSessionById(sessionId);
+
+    if (currentSession.hintsUsed >= validEnv.MAX_HINTS) {
+      throw new MonoalphabeticNoHintsLeftError("Maximum number of hints reached for this session.");
+    }
+    await this.monoalphabeticRepository.updateSession(
+      sessionId,
+      createExpirationDate(),
+      true,
+    );
+    const correctLetter: string = findCorrectLetterFromMapping(
+      requestedLetter,
+      currentSession.encryptionMapping,
+    );
+    return correctLetter;
+  }
 
   public async validateMapping(
     sessionId: string,
